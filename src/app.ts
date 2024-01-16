43cf5eea6682d9c04b13035542af50489e1abf52
Cleyson <cleysonph@gmail.com>
2024-01-15 22:09:14 -0300

    add error in tests

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { ZodError } from 'zod';
 import { apiRoutes } from './api/routes';
 import { NotFoundError } from './core/errors/not-found-error';
 
-  export const app = fastify();
+export const app = fastify();
 
 app.register(apiRoutes, { prefix: '/api' });
 
@@ -18,7 +18,7 @@ app.setErrorHandler(
       }));
       reply
         .status(400)
-        .send({ message: 'Validation error', errors });
+        .send({ message: 'Validation error!', errors });
     } else {
       console.error(error);
     }
